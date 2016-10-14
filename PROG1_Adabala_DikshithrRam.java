1c54d6828188ffe7be386a752d3fd39a93ecccf5
Dikshit-Ram <dikshit.ram02@gmail.com>
2016-10-14 12:33:46 -0500

    Add files via upload

---
+++ PROG1_Adabala_DikshithrRam.java
@@ -0,0 +1,36 @@
+
+public class PROG1_Adabala_DikshithrRam {
+
+
+	public static void main(String[] args) {
+		
+				System.out.println("The output of the program is");
+				int i;
+				for(i = 1;i < 100;i++)
+				{
+					if(i%3==0||i%5==0)						//if the number is divided by either 3 or 5 it enters braces
+					{
+						if(i%3==0&&i%5!=0)					//if the number is divided only by 3 then it prints "Hello"
+						{
+							System.out.println("Hello");
+						}
+						if(i%5==0&&i%3!=0)					//if the number is divided only by 5 then it prints "WIU"
+						{
+							System.out.println("WIU");
+						}
+						if(i%3==0&&i%5==0)					//if the number is divided both by 3 and 5 then it prints "Hello WIU"
+						{
+							System.out.println("Hello WIU");
+						}
+					}
+					else
+					{
+						System.out.println(+i);				//if the number is not divisible by either 3 or 5 then it prints the number itself
+					}
+					
+				}			
+	
+		}
+}
+
+
